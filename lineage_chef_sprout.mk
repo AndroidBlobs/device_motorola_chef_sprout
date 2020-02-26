e7bc69efc4d7ac11e875036d6267e5512668de50
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-26 08:58:28 +0000

    10 QPTS30.61-18-3 9514c release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ lineage_chef_sprout.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from chef_sprout device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := motorola
+PRODUCT_DEVICE := chef_sprout
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_NAME := lineage_chef_sprout
+PRODUCT_MODEL := motorola one power
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+TARGET_VENDOR := motorola
+TARGET_VENDOR_PRODUCT_NAME := chef_sprout
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC=" 10 QPTS30.61-18-3 9514c release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := motorola/chef_retail/chef_sprout:10/QPTS30.61-18-3/9514c:user/release-keys
